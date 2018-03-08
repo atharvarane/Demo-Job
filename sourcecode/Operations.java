f1ed731d9d1ce8c0a49c4003bf9f3d61c7fdf4a5
atharvarane <atharva.rane@ucdenver.edu>
2018-03-08 14:57:55 -0600

    Unit Tests Added

--- sourcecode/Operations.java
+++ sourcecode/Operations.java
@@ -1,4 +1,10 @@
-import java.lang.Math;
+import org.junit.Test;
+
+import java.lang.*;
+import java.util.Scanner;
+import org.junit.Assert;
+import org.junit.Test;
+
 
 public class Operations
 {
@@ -7,22 +13,27 @@ public class Operations
         int sum = a + b;
         System.out.println ("Total is : " + sum);
     }
-
-    public static void minus(int c, int d)
+    @Test
+    public void minus()
     {
+        String str ="Hello World";
+        Assert.assertEquals("Hello World", str);
+        int c = 20;
+        int d = 5;
         int diff = c - d;
         System.out.println ("Difference is : " + diff);
+        Assert.assertEquals(15, diff);
     }
     public static void multiply (int e, int f)
     {
         int mul = e * f;
         System.out.println("Multiplication is : " + mul);
     }
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         total(2,2);
-        minus(20,5);
+        //minus();
         multiply(2,5);
     }
 }
+
